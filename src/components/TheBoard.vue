6099519628b870d23484aeb391775c7a342b0a41
Abdumalik <axtrem13@gmail.com>
2025-09-29 23:00:46 +0500

    Border moved to its own component

---
+++ src/components/TheBoard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gray-900 w-4/5 working-area rounded-lg min-h-[70vh]">
+        
+    </div>
+</template>
--- src/components/TheWorkingArea.vue
+++ src/components/TheWorkingArea.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
+<script setup>
 import TheSidebar from './TheSidebar.vue';
-
+import TheBoard from './TheBoard.vue';
 </script>
 
 <template>
     <div class="flex my-5 gap-2 items-start">
         <TheSidebar class="w-1/5"/>
-        <div class="bg-gray-900 w-4/5 working-area rounded-lg min-h-[70vh]">
-            
-        </div>
+        <TheBoard/>
     </div>
 </template>
